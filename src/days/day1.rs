163f98d8fee8fd840272b3dd65d2f5b8e6176ebb
Rahul Garg <vihu89@gmail.com>
2019-02-18 00:48:15 -0800

    Getting even more concise, losing readability?

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -3,13 +3,9 @@ use std::{
     io::{BufRead, BufReader, Result},
 };
 
-fn lines_to_string_vector() -> Result<Vec<String>> {
-    BufReader::new(File::open("src/days/input_day1.txt")?).lines().collect()
-}
-
 pub fn answer() -> Result<i32> {
-    let vec = lines_to_string_vector()?;
-    let sum = vec.iter().fold(0, |sum, value| sum + value.parse::<i32>().unwrap());
-    Ok(sum)
+    let res = BufReader::new(File::open("src/days/input_day1.txt")?)
+                        .lines()
+                        .fold(0, |sum, value| sum + value.unwrap().parse::<i32>().unwrap());
+    Ok(res)
 }
-
